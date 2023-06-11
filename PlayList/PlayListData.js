b64bb777ab0b1737f1a95e65d04228eafab5bbf7
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2023-06-11 17:21:13 +0530

    commit 2 11-06-2023

---
+++ PlayList/PlayListData.js
@@ -0,0 +1,34 @@
+export const PlayListData = [
+  {
+    id: "An id",
+    title: "A title",
+    thumbnail: "A thumbnail",
+    url: "A url",
+    type: " A type",
+    description: "A description",
+  },
+  {
+    id: "An id",
+    title: "A title",
+    thumbnail: "A thumbnail",
+    url: "A url",
+    type: " A type",
+    description: "A description",
+  },
+  {
+    id: "An id",
+    title: "A title",
+    thumbnail: "A thumbnail",
+    url: "A url",
+    type: " A type",
+    description: "A description",
+  },
+  {
+    id: "An id",
+    title: "A title",
+    thumbnail: "A thumbnail",
+    url: "A url",
+    type: " A type",
+    description: "A description",
+  },
+];
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 // import { VideoPlayer } from "./VideoPlayer/VideoPlayer.js";
+import { PlayListData } from "./PlayList/PlayListData.js";
 
 const VideoPlayer = () => {
-  return <div className="md:w-1/2 h-1/2 md:h-full border-3 ">Video Player</div>;
+  return (
+    <div className="md:w-1/2 h-1/2 md:h-full flex md:justify-center md:items-center">
+      Video Player
+    </div>
+  );
 };
 
 const PlayList = () => {
-  return (
-    <div className="md:w-1/2 h-1/2 md:h-full border-3">
-      PlayList
+  console.log(PlayListData);
+  const PlayListCards = PlayListData.map((values, key) => (
+    <div
+      className="playListCard px-3 py-3 w-[97%] md:w-[47.5%] h-[125px] md:h-[150px] bg-emerald-900 hover:bg-lime-800 hover:cursor-pointer rounded-lg"
+      key={key}
+    >
+      <p>{values.id}</p>
+      <p>{values.title}</p>
+      <p>{values.description}</p>
+      <p>{values.url}</p>
     </div>
-  )
-}
+  ));
+  return (
+    <div className="px-4 md:w-1/2 h-1/2 md:h-full flex md:justify-center md:items-center">
+      <div className="md:h-3/5 flex flex-wrap gap-x-3 gap-y-2 overflow-y-auto playListCardsContainer">
+        {PlayListCards}
+        {PlayListCards}
+      </div>
+      {/* {PlayListCards} */}
+    </div>
+  );
+};
 
 const Component = () => {
   return (
