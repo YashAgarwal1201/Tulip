02999aee93c79bdebb6adcf7097067b6e0d28181
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2023-07-08 23:27:12 +0530

    commit 1 08-07-2023

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 // import { VideoPlayer } from "./VideoPlayer/VideoPlayer.js";
 import { PlayListData } from "./PlayList/PlayListData.js";
 const { useState } = React;
+const { Sidebar } = primereact.sidebar;
 
-const VideoPlayer = ({ video }) => {
-  // console.log(video);
+const VideoPlayer = ({ video, sidePlaylistMenu, setSidePlaylistMenu }) => {
+  // console.log(sidePlaylistMenu);
   return (
-    <div className="w-full lg:w-2/3 h-full flex  snap-center snap-always">
-      <div className="p-3 w-full h-full md:h-auto flex flex-col gap-y-3 bg-emerald-900 rounded-lg overflow-hidden">
-        {video?.type !== "video/Youtube" ? (
-          <video
-            key={video?.url}
-            className="w-full h-full rounded-lg bg-black"
-            controls
-          >
-            <source src={video?.url} type={video?.type} />
-            <p>Sorry, but some error has occured.</p>
-          </video>
-        ) : (
-          <iframe
-            className="w-full h-full rounded-lg border-2"
-            title={video?.title}
-            // src={video?.url}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        )}
-        <div className="p-3 w-full h-[200px] flex flex-col gap-y-3 item-start lg:items-center bg-rose-100 text-teal-900 rounded-lg relative">
-          <h3 className="w-full text-2xl capitalize font-medium">{video?.title}</h3>
-          <p className="w-full hidden md:block">
-            {video?.description ? video?.description : "No description"}
-          </p>
-          <span className="p-3 block lg:hidden material-symbols-rounded h-fit bg-teal-900 text-rose-100 hover:bg-rose-600 hover:cursor-pointer rounded-full absolute bottom-3 right-3">
-            expand_more
-          </span>
-        </div>
+    // <div className="w-full lg:w-2/3 h-full flex  snap-center snap-always">
+    <div className="p-3 w-full h-full md:h-auto flex flex-col gap-y-3 bg-emerald-900 rounded-lg overflow-hidden">
+      {video?.type !== "video/Youtube" ? (
+        <video
+          key={video?.url}
+          className="w-full h-full aspect-video rounded-lg bg-black"
+          controls
+        >
+          <source src={video?.url} type={video?.type} />
+          <p>Sorry, but some error has occured.</p>
+        </video>
+      ) : (
+        <iframe
+          className="w-full h-full aspect-video rounded-lg border-2"
+          title={video?.title}
+          src={video?.url}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      )}
+      <div className="p-3 w-full h-[200px] flex flex-col gap-y-3 item-start lg:items-center bg-rose-100 text-teal-900 rounded-lg relative">
+        <h3 className="w-full text-2xl capitalize font-medium">
+          {video?.title}
+        </h3>
+        <p className="w-full hidden md:block">
+          {video?.description ? video?.description : "No description"}
+        </p>
+        <span
+          className="p-3 block lg:hidden material-symbols-rounded h-fit bg-teal-900 text-rose-100 hover:bg-rose-600 hover:cursor-pointer rounded-full absolute bottom-3 right-3"
+          onClick={() => setSidePlaylistMenu(true)}
+        >
+          playlist_play
+        </span>
       </div>
     </div>
+    // </div>
   );
 };
 
-const PlayList = ({ PlayListData, selectedVideo, setSelectedVideo }) => {
+const PlayList = ({
+  PlayListData,
+  selectedVideo,
+  setSelectedVideo,
+  sidePlaylistMenu,
+  setSidePlaylistMenu,
+}) => {
+  // console.log(sidePlaylistMenu);
   const [playListView, setPlayListView] = useState("grid");
   const PlayListCards = PlayListData?.map((values, key) => (
     <div
@@ -50,7 +63,10 @@ const PlayList = ({ PlayListData, selectedVideo, setSelectedVideo }) => {
         selectedVideo === key ? "bg-lime-800" : "bg-emerald-900"
       } hover:bg-lime-800 hover:cursor-pointer rounded-lg`}
       key={key}
-      onClick={() => setSelectedVideo(key)}
+      onClick={() => {
+        setSelectedVideo(key);
+        sidePlaylistMenu ? setSidePlaylistMenu(false) : "";
+      }}
     >
       {!values?.thumbnail ? (
         <img
@@ -80,7 +96,10 @@ const PlayList = ({ PlayListData, selectedVideo, setSelectedVideo }) => {
     </div>
   ));
   return (
-    <div className="w-full lg:w-1/3 h-full flex flex-col gap-y-3  snap-start snap-always">
+    <>
+      {/* <div
+        className={`w-full lg:w-1/3 h-full flex flex-col gap-y-3  snap-start snap-always`}
+      > */}
       <div className="p-2 w-full text-2xl flex justify-between items-center bg-emerald-900 rounded-lg">
         <h2>Playlist</h2>
         <div className="flex gap-x-2">
@@ -116,20 +135,54 @@ const PlayList = ({ PlayListData, selectedVideo, setSelectedVideo }) => {
         {PlayListCards}
         {PlayListCards} */}
       </div>
-    </div>
+      {/* </div> */}
+    </>
   );
 };
 
 const Component = ({ PlayListData }) => {
   const [selectedVideo, setSelectedVideo] = useState(0);
+  const [sidePlaylistMenu, setSidePlaylistMenu] = useState(false);
   return (
     <div className="w-full h-full flex flex-wrap lg:flex-nowrap flex-column lg:flex-row p-3 gap-3 overflow-y-auto lg:overflow-hidden snap-y snap-mandatory">
-      <VideoPlayer video={PlayListData[selectedVideo]} />
-      <PlayList
-        PlayListData={PlayListData}
-        selectedVideo={selectedVideo}
-        setSelectedVideo={setSelectedVideo}
-      />
+      <div className="w-full lg:w-2/3 h-full flex  snap-center snap-always">
+        <VideoPlayer
+          video={PlayListData[selectedVideo]}
+          sidePlaylistMenu={sidePlaylistMenu}
+          setSidePlaylistMenu={setSidePlaylistMenu}
+        />
+      </div>
+      <div
+        className={`w-full lg:w-1/3 h-full hidden lg:flex flex-col gap-y-3  snap-start snap-always`}
+      >
+        <PlayList
+          PlayListData={PlayListData}
+          selectedVideo={selectedVideo}
+          setSelectedVideo={setSelectedVideo}
+        />
+      </div>
+      <div>
+        <Sidebar
+          className="block lg:hidden bg-teal-950 text-rose-100 w-[90vw]"
+          visible={sidePlaylistMenu}
+          position="right"
+          onHide={() => setSidePlaylistMenu(false)}
+          modal={false}
+          dismissable
+        >
+          <div
+            className={`w-full h-full flex flex-col gap-y-3  snap-start snap-always`}
+          >
+            <PlayList
+              PlayListData={PlayListData}
+              selectedVideo={selectedVideo}
+              setSelectedVideo={setSelectedVideo}
+              sidePlaylistMenu={sidePlaylistMenu}
+              setSidePlaylistMenu={setSidePlaylistMenu}
+            />
+          </div>
+        </Sidebar>
+      </div>
     </div>
   );
 };
